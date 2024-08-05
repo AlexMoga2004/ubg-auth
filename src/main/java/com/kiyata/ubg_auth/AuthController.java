9b4e00243ba4d3aac151d639a6e2d555bd929e3f
AlexMoga2004 <alexmoga258@gmail.com>
2024-08-05 15:06:17 +0100

    improved existing email response

--- src/main/java/com/kiyata/ubg_auth/AuthController.java
+++ src/main/java/com/kiyata/ubg_auth/AuthController.java
@@ -5,11 +5,10 @@ import com.kiyata.ubg_auth.user.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -19,13 +18,18 @@ public class AuthController {
     @Autowired
     UserService userService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/register")
-    public ResponseEntity<String> register(@Valid @RequestBody User user) {
+    public ResponseEntity<?> register(@Valid @RequestBody User user) {
         Optional<User> registeredUser = userService.registerUser(user);
         if (registeredUser.isPresent()) return ResponseEntity.ok("User registered successfully");
-        return ResponseEntity.status(401).body("User already registered with this email");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("email", "User already registered with this email");
+        return ResponseEntity.status(401).body(response);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User loginRequest) {
         String email = loginRequest.getEmail();
